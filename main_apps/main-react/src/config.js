85de52ac3273eba8843bc521bf54c8c0ee533ea9
ruanxusong <ruanxusong@thundersdata.com>
2022-08-16 14:06:06 +0800

    fix: 调整 thunderBi 请求地址

--- main_apps/main-react/src/config.js
+++ main_apps/main-react/src/config.js
@@ -9,7 +9,7 @@ const isEnvPro = process.env.NODE_ENV === 'production';
 if (isEnvPro) {
   const locationOrigin = `${location.origin}/`;
   config = {
-    thunderBi: 'http://smart-bi.jxzk.thundersdata.com:38204/',
+    thunderBi: 'http://smart-bi.jxzk.thundersdata.com:38204/thunderBi',
     dataFront: 'http://data-sso.dev.thundersdata.com/',
     biSheng: 'http://lowcode.jxzk.thundersdata.com:38204/',
     swiftScreen: 'http://smart-visualization.jxzk.thundersdata.com:38204/',
