629027292f6860b484262e1d0db18bf7295ef3d1
陈英杰 <13487079308@163.com>
2022-08-31 14:02:47 +0800

    feat: 增加主题切换

--- main_apps/main-react/src/components/GlobalHeader/AvatarDropdown.jsx
+++ main_apps/main-react/src/components/GlobalHeader/AvatarDropdown.jsx
@@ -1,16 +1,31 @@
 import { LOGOUT_PATH } from '@/config';
-import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { LogoutOutlined, SettingOutlined, UserOutlined ,FormatPainterOutlined} from '@ant-design/icons';
 import { Avatar, Menu, Spin } from 'antd';
 import React from 'react';
 import { history, connect } from 'umi';
 import HeaderDropdown from '../HeaderDropdown';
 import styles from './index.less';
 import microApp from '@micro-zoe/micro-app';
+import { enable as enableDarkMode, disable as disableDarkMode } from 'darkreader';
 
 class AvatarDropdown extends React.Component {
   state = {
     showDropDown: false,
+    dark:false,
   };
+
+  changeTheme(){
+   if (this.state.dark) {
+    enableDarkMode({
+      brightness: 100,
+      contrast: 90,
+      sepia: 10,
+    });
+    return;
+  }
+  disableDarkMode();
+  }
+
   onMenuClick = (event) => {
     const { key } = event;
 
@@ -26,8 +41,18 @@ class AvatarDropdown extends React.Component {
       return;
     }
 
+    if(key ==='changeTheme'){
+      this.setState({ dark: !this.state.dark },()=>{
+        localStorage.setItem('dark', this.state.dark);
+        this.changeTheme();
+      });
+      return;
+    }
+
     history.push(`/account/${key}`);
   };
+
+  
 
   handleGlobalDataForBaseApp = (data) => {
     this.setState({ showDropDown: data.showDropDown });
@@ -35,6 +60,12 @@ class AvatarDropdown extends React.Component {
 
   componentDidMount() {
     microApp.addGlobalDataListener(this.handleGlobalDataForBaseApp);
+
+    const dark = localStorage.getItem("dark");
+    this.setState({ dark: dark=="true" },()=>{
+      this.changeTheme();
+    });
+
   }
 
   render() {
@@ -49,10 +80,12 @@ class AvatarDropdown extends React.Component {
       <Menu
         className={styles.menu}
         selectedKeys={[]}
-        onClick={() => {
-          window.location.href = LOGOUT_PATH;
-        }}
+        onClick={this.onMenuClick}
       >
+        <Menu.Item key="changeTheme">
+          <FormatPainterOutlined /> 
+          切换主题
+        </Menu.Item>
         <Menu.Item key="logout">
           <LogoutOutlined />
           退出登录
